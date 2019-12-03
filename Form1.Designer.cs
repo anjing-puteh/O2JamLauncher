4926977b0ce4cba480cac5262ccea1d1463d7c23
ghofars302 <ghofarsuprapto@gmail.com>
2019-12-03 13:58:25 +0700

    Fist commit!

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace O2JamLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(548, 263);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.TabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(540, 237);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.checkBox1);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(540, 237);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(8, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Window Mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Settings";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 295);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Null";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace O2JamLauncher
+{
+    public partial class Form1 : Form
+    {
+        // DLLs Load
+        [DllImport("user32", CharSet = CharSet.Ansi, EntryPoint = "FindWindowA", ExactSpelling = true, SetLastError = true)]
+        private static extern IntPtr FindWindow(ref string lpClassName, ref string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        private static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32", CharSet = CharSet.Auto, ExactSpelling = false, SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        // Basic Declaration
+        public Timer Timer1 = new Timer();
+
+        // LauncherBasicConfig
+        public string ProcessName = "O2-JAM";
+        public string WinX = "800";
+        public string WinY = "600";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "MonoxJam Launcher";
+            tabPage1.Text = "Home";
+            tabPage2.Text = "Settings";
+
+            Timer1.Tick += new EventHandler(Timer1_Tick);
+            Timer1.Interval = 0;
+        }
+
+        private void TabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(Application.StartupPath + "\\otwo.exe")) {
+                MessageBox.Show("otwo.exe not detected in directory nor process!", "Error");
+                return;
+            }
+
+            Process app = new Process();
+            app.StartInfo.FileName = Application.StartupPath + "\\otwo.exe";
+            app.StartInfo.Arguments = "";
+            app.Start();
+            Timer1.Start();
+            Hide();
+
+            app.WaitForExit();
+            Show();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] processesByName = Process.GetProcessesByName("otwo");
+            try
+            {
+                string mainWindowTitle = processesByName[0].MainWindowTitle;
+                IntPtr intPtr1 = FindWindow(ref ProcessName, ref mainWindowTitle);
+                SetWindowPos(intPtr1, (int)(new IntPtr(-1)), 0, 0, Conversions.ToInteger(WinX), Conversions.ToInteger(WinY), 2);
+                if (Conversions.ToBoolean(processesByName[0].MainWindowTitle.Contains("O2-JAM").ToString().ToUpper()))
+                {
+                    Timer1.Enabled = false;
+                }
+            } catch (Exception error)
+            {
+                ProjectData.SetProjectError(error);
+                ProjectData.ClearProjectError();
+            }
+        }
+    }
+}
